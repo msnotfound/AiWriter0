cd052595c50f14168c3b657a8ac2b6c42c0ed6b2
msnotfound <gca1245@gmail.com>
2024-11-30 03:50:07 +0530

    added necessary files for  netlify

--- server/netlify/functions/server.js
+++ server/netlify/functions/server.js
@@ -2,11 +2,11 @@ const express = require("express");
 const mongoose = require("mongoose");
 // const passport = require("passport");
 const session = require("express-session");
-const authRoutes = require("../routes/auth");
+const authRoutes = require("../../routes/auth");
 
 require("dotenv").config();
 const cors = require("cors");
-const User = require('../models/User');
+const User = require('../../models/User');
 
 
 const app = express();
